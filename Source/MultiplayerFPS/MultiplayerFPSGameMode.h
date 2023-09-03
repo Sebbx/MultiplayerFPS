1c5164055d66656a1c2b0347775be19554b60bc6
Sebastian <s.rubacha@actum.games>
2023-09-03 15:18:12 +0200

    Project creation from FPS template

---
+++ Source/MultiplayerFPS/MultiplayerFPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerFPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiplayerFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiplayerFPSGameMode();
+};
+
+
+
